1d8d9ab1d904a1a808aa6e653c2ff15697c9dcbd
PabloLds <>
2022-10-19 10:26:56 -0300

    aaaaaa

--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
 import express from 'express';
-import { routes } from '../routes.js';
+import cors from "cors";
+import { routes } from './routes.js';
 const app = express();
 
+const port = process.env.PORT = 4000
+
+
+
 app.use(express.json());
-app.use(routes);
+//app.use(routes);
 
 app.listen(8081, () => {
     console.log("servidor rodando")
